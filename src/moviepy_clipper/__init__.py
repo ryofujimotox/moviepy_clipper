980bc931da08cb7bcea5f43a62fc08effa2962ba
ryo.fujimoto <ryo.fujimoto@caters.co.jp>
2023-02-17 14:03:47 +0900

    feat: neon.mp4

--- src/moviepy_clipper/__init__.py
+++ src/moviepy_clipper/__init__.py
@@ -3,5 +3,6 @@ from .image import _image
 from .repeat_mp4 import RepeatVideo
 from .scrolling_text import ScrollingText, getDurationTextScroll
 from .resize import *
+from .neon_mp4 import *
 
 __version__ = "0.1.0"
---
+++ src/moviepy_clipper/neon_mp4.py
@@ -0,0 +1,53 @@
+from moviepy.editor import CompositeVideoClip
+from .repeat_mp4 import RepeatVideo
+from .resize import resizeFit
+from .scrolling_text import ScrollingText
+
+from .audio import mergeAudio
+
+
+def neon_mp4(
+    source: str,
+    duration: int = 0,
+    size: list = None,
+    scrolling_text: str = None,
+    bgm: str = None,
+):
+    """
+    繰り返し再生を続けるclipを返す。
+    再生時間(duration)を指定していない場合は、1回だけ流す。
+
+    Parameters
+    -----------
+    source
+        ビデオまでのパス。
+    duration
+        再生時間。指定していない場合は、1回だけ流す。
+    size
+        (width, heigth) fitさせる。余白は黒色
+    scrolling_text
+        右下から左下をスクロールするテキスト
+    bgm
+        音楽を流す
+    """
+
+    # 動画クリップ
+    clip = RepeatVideo(source, duration)
+
+    # リサイズ
+    if size:
+        clip = resizeFit(clip, size, color=[0, 0, 0])
+
+    # 字幕をつける
+    if scrolling_text:
+        stream_range = clip.size[0]
+        txt_clip = ScrollingText(
+            text=scrolling_text, duration=clip.duration, stream_range=stream_range
+        )
+        clip = CompositeVideoClip([clip, txt_clip])
+
+    # BGMをつける
+    if bgm:
+        clip = mergeAudio(clip, bgm)
+
+    return clip
